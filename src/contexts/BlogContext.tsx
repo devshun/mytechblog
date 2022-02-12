6a798b1a5328b4b481429714aebee698cb487582
shunppe <70781900+shunppe@users.noreply.github.com>
2022-02-13 06:24:27 +0900

    add Blog context

--- src/contexts/BlogContext.tsx
+++ src/contexts/BlogContext.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 import { BlogItemType, BlogDataType } from '../types';
 
 import { initialBlogData } from '../constants/initialState';
+
+export type BlogStateType = {
+  blogList: BlogItemType[];
+  totalCount: number;
+};
+
+const initialState: Readonly<BlogStateType> = {
+  blogList: [],
+  totalCount: 0,
+};
 
 const ActionType = {
   SET: 'SET_BLOG',
@@ -37,3 +47,20 @@ const BlogStateContext = React.createContext(initialBlogData as BlogDataType);
 const BlogDispatchContext = React.createContext<BlogDispatchType>(() => {
   throw new TypeError('Context not provided');
 });
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export const BlogContextProvider: React.FC<Props> = ({ children }: Props) => {
+  const [state, dispatch] = useReducer(BlogReducer, initialState);
+
+  return (
+    <BlogDispatchContext.Provider value={dispatch}>
+      <BlogStateContext.Provider value={state}>{children}</BlogStateContext.Provider>
+    </BlogDispatchContext.Provider>
+  );
+};
+
+export const useBlogState = () => React.useContext(BlogStateContext);
+export const useBlogDispatch = () => React.useContext(BlogDispatchContext);
