5676d4305eb6968373e30e2677a65b7cf98a0c4b
shunppe <70781900+shunppe@users.noreply.github.com>
2022-02-14 07:48:07 +0900

    fix Textをリファクタ

--- src/components/common/atoms/Text.tsx
+++ src/components/common/atoms/Text.tsx
@@ -2,13 +2,12 @@ import React, { HTMLAttributes, memo } from 'react';
 
 export type Props = HTMLAttributes<HTMLDivElement> & {
   children: React.ReactNode;
-  size: 'small' | 'medium' | 'large';
-  color: 'red';
+  size?: 'small' | 'medium' | 'large';
 };
 
 export const Text: React.FC<Props> = memo((props) => {
-  const { children, className, size ="medium", color, ...rest } = props;
-  const classes = [className, sizeClasses[size], colorClasses[color]].join(' ');
+  const { children, className, size = 'medium', ...rest } = props;
+  const classes = [className, size && sizeClasses[size]].join(' ');
   return (
     <div className={classes} {...rest}>
       {children}
@@ -18,10 +17,6 @@ export const Text: React.FC<Props> = memo((props) => {
 
 export const sizeClasses = {
   small: 'text-sm',
-  medium: 'text-lg',
+  medium: 'text-lg font-bold',
   large: 'text-2xl font-bold',
-};
-
-export const colorClasses = {
-  red: '',
 };
