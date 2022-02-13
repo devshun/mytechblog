e54d841e65d05ff4efc134a60c4dfd118ecb1f68
shunppe <70781900+shunppe@users.noreply.github.com>
2022-02-13 13:18:44 +0900

    fix header

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Button } from '../common/atoms/Button';
+import { Text } from '../common/atoms/Text';
 
-type Props = {
-
-}
-
+type Props = {};
 
 export const Header: React.FC<Props> = () => {
-  return <div></div>;
+  return (
+    <div className='fixed top-0 left-0 right-0 z-999 bg-neutral-900 flex justify-between items-center border-b h-16 p-5'>
+      <Text size='medium'>devshun's Tech Blog</Text>
+      <Button>Author's Profile</Button>
+    </div>
+  );
 };
