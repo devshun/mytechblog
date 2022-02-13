f9f8deb0d8750e014a51d0900466ffc86d66120c
shunppe <70781900+shunppe@users.noreply.github.com>
2022-02-14 07:50:18 +0900

    add blogItemのスタイルを適用

--- src/components/common/molecules/BlogItem.tsx
+++ src/components/common/molecules/BlogItem.tsx
@@ -15,21 +15,24 @@ export const BlogItem: React.FC<Props> = memo((props: Props) => {
 
   return (
     <Link href='[blogId]' as={`/${blogItem.id}`}>
-      <div className='h-80 w-72 mb-10 flex flex-col  p-5 border rounded-2xl cursor-pointer bg-neutral-800 mx-auto'>
-        <div className='p-2'>
+      <div className='h-80 w-72 mb-10 flex flex-col justify-between  p-5 border rounded-2xl cursor-pointer bg-neutral-800 mx-auto hover:scale-105 transition-all'>
+        <div className=''>
           <Text>{blogItem.title}</Text>
         </div>
-        {/* <div className='hover:scale-110 transition-all'>
+        <div className=''>
           <img
-            src={"https://shop38-makeshop.akamaized.net/shopimages/pgsentame/000000078988_vIep3W4.jpg"}
+            className='mx-auto rounded-md'
+            src={
+              'https://shop38-makeshop.akamaized.net/shopimages/pgsentame/000000078988_vIep3W4.jpg'
+            }
             alt='Picture'
             // width={blogItem.image.width * 2}
             // height={blogItem.image.height * 2}
-            width={"200px"}
-            height={"100px"}
-
+            width={'200px'}
+            height={'100px'}
           />
-        </div> */}
+        </div>
+        <Text className='text-gray-400' size='small'>icon</Text>
       </div>
     </Link>
   );
