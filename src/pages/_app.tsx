bdf95c2e331f1080902e9d73d1e38bae372c9456
shunppe <70781900+shunppe@users.noreply.github.com>
2022-02-13 10:25:04 +0900

    fix wraped provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
-import '../styles/globals.css'
-import 'tailwindcss/tailwind.css'
+import '../styles/globals.css';
+import 'tailwindcss/tailwind.css';
 
 import type { AppProps } from 'next/app';
+import { RootContextProvider } from '../contexts';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <RootContextProvider>
+      <Component {...pageProps} />
+    </RootContextProvider>
+  );
 }
 
 export default MyApp;
