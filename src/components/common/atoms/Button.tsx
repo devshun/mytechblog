7d57e352e8fb0a9ea51071e270d7efebc0daada2
shunppe <70781900+shunppe@users.noreply.github.com>
2022-02-15 21:57:18 +0900

    add Buttontest

--- src/components/common/atoms/Button.tsx
+++ src/components/common/atoms/Button.tsx
@@ -1,19 +1,21 @@
-import React, { ButtonHTMLAttributes } from 'react';
+import React, { ButtonHTMLAttributes, memo } from 'react';
 
 export type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
   children: React.ReactNode;
-  size: 'small' | 'medium' | 'large';
+  size?: 'small' | 'medium' | 'large';
 };
 
-export const Button: React.FC<Props> = (props) => {
-  const { children, className, size = "medium", ...rest } = props;
-  const classes = [className, sizeClasses[size]].join(' ');
+export const Button: React.FC<Props> = memo((props) => {
+  const { children, className, size = 'medium', ...rest } = props;
+  const classes = [baseClasses, className, size && sizeClasses[size]].join(' ');
   return (
     <button className={classes} {...rest}>
       {children}
     </button>
   );
-};
+});
+
+const baseClasses = 'bg-gray-100 text-black rounded-xl font-bold px-5 py-3';
 
 const sizeClasses = {
   small: '',
