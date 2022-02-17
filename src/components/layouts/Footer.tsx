8421e4752d90ff0626abb99666f046ab959f9201
shunppe <70781900+shunppe@users.noreply.github.com>
2022-02-18 00:23:17 +0900

    fix headerfooterスタイル修正

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -3,5 +3,5 @@ import React from 'react';
 type Props = {};
 
 export const Footer: React.FC<Props> = () => {
-  return <div></div>;
+  return <div className='h-32 flex justify-center items-center'>My Tech Blog @2022</div>;
 };
--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React, { useLayoutEffect } from 'react';
 import { Button } from '../common/atoms/Button';
 import { Text } from '../common/atoms/Text';
@@ -5,13 +6,14 @@ import { Text } from '../common/atoms/Text';
 type Props = {};
 
 export const Header: React.FC<Props> = () => {
- 
   return (
     <div className='fixed top-0 left-0 right-0 z-999 bg-neutral-900 flex justify-between items-center border-b h-20 p-5'>
       <Text className='ml-8' size='medium'>
-        Tech Blog
+        Shunya Hayashi's Tech Blog
       </Text>
-      <Button>Author's Profile</Button>
+      <Link href='profile'>
+        <Button>Author's Profile</Button>
+      </Link>
     </div>
   );
 };
