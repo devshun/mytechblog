a740235d70edb0185ac80fa761103b73d53c45ec
shunppe <70781900+shunppe@users.noreply.github.com>
2022-02-17 23:40:07 +0900

    fix BlogItemのスタイルを修正

--- src/components/common/molecules/BlogItem.tsx
+++ src/components/common/molecules/BlogItem.tsx
@@ -6,6 +6,8 @@ import { BlogItemType } from '../../../types';
 
 import { Text } from '../atoms/Text';
 
+import { format, parseISO } from 'date-fns';
+
 type Props = {
   blogItem: BlogItemType;
 };
@@ -13,26 +15,27 @@ type Props = {
 export const BlogItem: React.FC<Props> = memo((props: Props) => {
   const { blogItem } = props;
 
+  console.log(blogItem);
   return (
     <Link href='[blogId]' as={`/${blogItem.id}`}>
-      <div className='h-80 w-72 mb-10 flex flex-col justify-between  p-5 border rounded-2xl cursor-pointer bg-neutral-800 mx-auto hover:scale-105 transition-all'>
-        <div className=''>
-          <Text>{blogItem.title}</Text>
+      <div className='h-80 w-80 mb-10 flex flex-col justify-between border rounded-2xl cursor-pointer bg-neutral-800 mx-auto hover:scale-105 transition-all'>
+        <div className='h-2/5 flex flex-col justify-between my-3'>
+          <Text className='px-8 py-6'>{blogItem.title}</Text>
+          <Text size='xsmall' className='text-gray-400 text-center'>
+            {format(parseISO(blogItem.publishedAt), 'yyyy/MM/dd')}
+          </Text>
         </div>
-        <div className=''>
-          <img
-            className='mx-auto rounded-md'
-            src={
-              'https://shop38-makeshop.akamaized.net/shopimages/pgsentame/000000078988_vIep3W4.jpg'
-            }
-            alt='Picture'
-            // width={blogItem.image.width * 2}
-            // height={blogItem.image.height * 2}
-            width={'200px'}
-            height={'100px'}
-          />
+        <div className='overflow-hidden flex mx-auto items-center h-3/5 w-9/10 p-3'>
+          {blogItem.image && (
+            <Image
+              className='rounded-md'
+              src={blogItem.image.url}
+              alt='Picture'
+              width={blogItem.image.width * 2}
+              height={blogItem.image.height * 2}
+            />
+          )}
         </div>
-        <Text className='text-gray-400' size='small'>icon</Text>
       </div>
     </Link>
   );
