e3b39d15072a0848e75cb92f01a4cf48172b437c
shunppe <70781900+shunppe@users.noreply.github.com>
2022-02-20 20:27:18 +0900

    add Buttonのテストを追加

--- src/components/common/atoms/Button.spec.tsx
+++ src/components/common/atoms/Button.spec.tsx
@@ -1,1 +1,45 @@
-// test of button
+import '@testing-library/jest-dom/extend-expect';
+import React from 'react';
+import {
+  cleanup,
+  fireEvent,
+  render,
+  RenderResult,
+  screen,
+} from '@testing-library/react';
+
+import { Button, Props } from '@/components/common/atoms/Button';
+
+afterEach(cleanup);
+
+describe('【コンポーネントテスト】 Button', () => {
+  const renderComponent = (props: Partial<Props> = {}): RenderResult => {
+    const defaultProps = {
+      children: 'テストボタンテキスト',
+    };
+    return render(<Button {...defaultProps} {...props} />);
+  };
+
+  it('should be render with children', async () => {
+    renderComponent();
+    const button = await screen.findByRole('button');
+    // role = "button" として存在しているか
+    expect(button).toBeInTheDocument();
+    // childrenが正常に渡っているか
+    expect(button.textContent).toMatch('テストボタンテキスト');
+  });
+
+  //  todo
+  it('should be apply size style', async () => {});
+
+  //  todo
+  it('should be apply disabled props', async () => {});
+
+  //   onClick eventのバインドテスト
+  it('should be call onClick event', async () => {
+    const mockOnClick = jest.fn();
+    renderComponent({ onClick: mockOnClick });
+    fireEvent.click(await screen.findByRole('button'));
+    expect(mockOnClick).toHaveBeenCalled();
+  });
+});
