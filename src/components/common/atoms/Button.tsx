89db2f296a4f4545b133db5bdf5e1052432b9120
shunppe <70781900+shunppe@users.noreply.github.com>
2022-02-24 01:52:56 +0900

    fix Button TextをLink対応

--- src/components/common/atoms/Button.tsx
+++ src/components/common/atoms/Button.tsx
@@ -1,21 +1,26 @@
 import React, { ButtonHTMLAttributes, forwardRef, ReactElement } from 'react';
-
+import Link from 'next/link';
 import classNames from 'classnames';
 
 export type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
   children: React.ReactNode;
   size?: 'small' | 'medium' | 'large';
+  url?: string;
 };
 
 export const Button = forwardRef<HTMLButtonElement, Props>(
   (props, ref?): ReactElement => {
-    const { children, className, size = 'medium', ...rest } = props;
+    const { children, className, size = 'medium', url, ...rest } = props;
     const classes = classNames(baseClasses, className, sizeClasses[size]);
 
-    return (
+    const renderButton = () => (
       <button className={classes} {...rest} ref={ref}>
         {children}
       </button>
+    );
+
+    return (
+      <>{url ? <Link href={url}>{renderButton()}</Link> : renderButton()}</>
     );
   }
 );
--- src/components/common/atoms/Text.tsx
+++ src/components/common/atoms/Text.tsx
@@ -1,22 +1,25 @@
 import React, { HTMLAttributes, forwardRef, ReactElement } from 'react';
 
 import classNames from 'classnames';
+import Link from 'next/link';
 
 export type Props = HTMLAttributes<HTMLDivElement> & {
   children: React.ReactNode;
   size?: 'xsmall' | 'small' | 'medium' | 'large';
+  url: string;
 };
 
 export const Text = forwardRef<HTMLDivElement, Props>(
   (props, ref?): ReactElement => {
-    const { children, className, size = 'medium', ...rest } = props;
+    const { children, className, size = 'medium', url, ...rest } = props;
     const classes = classNames(className, sizeClasses[size]);
 
-    return (
+    const renderText = () => (
       <div className={classes} {...rest} ref={ref}>
         {children}
       </div>
     );
+    return <>{url ? <Link href={url}>{renderText()}</Link> : renderText()}</>;
   }
 );
 
